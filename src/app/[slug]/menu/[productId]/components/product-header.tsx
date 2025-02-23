9015d4d6b13d119fb66bf0129efffad89236b06b
monterick <rick_-26@hotmail.com>
2025-02-22 23:38:09 -0300

    commit

--- src/app/[slug]/menu/[productId]/components/product-header.tsx
+++ src/app/[slug]/menu/[productId]/components/product-header.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 import { Product } from "@prisma/client";
 import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 
 interface ProductHeaderProps{
     product: Pick<Product, 'name' | 'imageUrl'>
@@ -14,6 +14,10 @@ const ProductHeader = ({product} : ProductHeaderProps) => {
     
     const router = useRouter();
     const handleBackClick = () => router.back();
+
+    const {slug} = useParams<{slug: string}>();
+
+    const handleOrdersClick = () => router.push(`/${slug}/orders`);
 
     return(
         <div className="relative w-full h-[360px]">
@@ -26,7 +30,7 @@ const ProductHeader = ({product} : ProductHeaderProps) => {
             fill
             className="object-contain"
         />
-        <Button variant="secondary" size="icon" className="absolute right-4 top-4 rounded-full z-50">
+        <Button variant="secondary" size="icon" className="absolute right-4 top-4 rounded-full z-50" onClick={handleOrdersClick}>
                 <ScrollTextIcon />
         </Button>
        </div>
